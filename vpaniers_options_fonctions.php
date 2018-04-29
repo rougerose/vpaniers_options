0cf0739159028cbc905694f80ac2d22a12fd9fc5
christophe le drean <christophe@rouge-rose.net>
2018-04-29 13:03:48 +0200

    Ajout formulaire valier_panier :
    Avec le formulaire, ajout également de chaînes de langues
    et de fonction de calcul de hash de sécurité pour le paiement de
    la commande

--- vpaniers_options_fonctions.php
+++ vpaniers_options_fonctions.php
@@ -4,7 +4,27 @@ if (!defined("_ECRIRE_INC_VERSION")) {
     return;
 }
 
-
-function timestamp_vers_date($timestamp) {
+/**
+ * Convertir un timestamp unix vers une date
+ * qui peut-être formatée par Spip au moyen du filtre |affdate
+ * 
+ * @param  string $timestamp
+ * @return string date Y-m-d
+ */
+function vpaniers_timestamp_vers_date($timestamp) {
 	return $date = date('Y-m-d', $timestamp);
+}
+
+
+/**
+ * Calculer un hash de sécurité pour le paiement d'une commande. 
+ * 
+ * @param  int $id_auteur
+ * @param  int $id_commande
+ * @param  int $date_commande
+ * @return string
+ */
+function vpaniers_calcul_hash_commande($id_auteur, $id_commande, $date_commande) {
+	$donnees = array($id_auteur, $id_commande, $date_commande);
+	return md5(implode(';', array_values($donnees)));
 }
