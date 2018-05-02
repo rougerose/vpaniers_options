7f345dfe3feea1a27abe8a541d6ab1773506816e
christophe le drean <christophe@rouge-rose.net>
2018-05-02 10:40:11 +0200

    Panier: afficher détails relatifs à l'abonnement

--- lang/vpaniers_fr.php
+++ lang/vpaniers_fr.php
@@ -4,19 +4,16 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'abonnement_resume_label_abonnement'        => "Abonnement ",
-	'abonnement_resume_label_abonnement_offrir' => "Offert ",
-	'abonnement_resume_label_beneficiaire' => "Abonnement offert à ",
+	'abonnement_resume_label_abonnement_offrir' => "Abonnement offert ",
+	'abonnement_resume_label_beneficiaire'      => "Abonnement offert à ",
 	'abonnement_resume_label_cadeau'            => "Votre cadeau ",
 	'abonnement_resume_label_numero'            => "À partir du numéro ",
 	'abonnement_resume_cadeau_vide'             => "aucun",
 	'abonnements_resume_titre'                  => "Votre choix",
-	
-	'erreur_technique_creation_commande' => "Une erreur technique a empêché la validation de votre commande",
-	'erreur_accepter_contrat' => "Vous devez accepter le contrat",
-	'erreur_id_panier' => "Il n'est pas possible d'identifier votre panier",
-	'erreur_id_auteur' => "Veuillez vous identifier ou créer votre compte.",
-	
-	'label_accepter_contrats' => "Avant de valider, veuillez accepter le contrat&nbsp;:",
-	
-	'message_panier_valide' => "Votre commande est validée",
+	'erreur_technique_creation_commande'        => "Une erreur technique a empêché la validation de votre commande",
+	'erreur_accepter_contrat'                   => "Vous devez accepter le contrat",
+	'erreur_id_panier'                          => "Il n'est pas possible d'identifier votre panier",
+	'erreur_id_auteur'                          => "Veuillez vous identifier ou créer votre compte.",
+	'label_accepter_contrats'                   => "Avant de valider, veuillez accepter le contrat&nbsp;:",
+	'message_panier_valide'                     => "Votre commande est validée",
 );
