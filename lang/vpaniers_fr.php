85d528e0fafe10c3ad5e7fc1b64ec2d77868a180
christophe le drean <christophe@rouge-rose.net>
2018-06-22 12:24:32 +0200

    Page panier

--- lang/vpaniers_fr.php
+++ lang/vpaniers_fr.php
@@ -14,13 +14,26 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'abonnement_resume_label_numero'            => "À partir du numéro ",
 	'abonnement_resume_cadeau_vide'             => "aucun",
 	'abonnements_resume_titre'                  => "Votre choix",
+	
 	'bouton_valider_panier' => "Valider votre panier",
+	
 	'erreur_technique_creation_commande'        => "Une erreur technique a empêché la validation de votre commande",
 	'erreur_accepter_contrat'                   => "Vous devez accepter le contrat",
 	'erreur_id_panier'                          => "Il n'est pas possible d'identifier votre panier",
 	'erreur_id_auteur'                          => "Veuillez vous identifier ou créer votre compte.",
-	'label_afficher_panier' => "Voir le panier",
+	'erreur_abonnement_incomplet' => "Une information est manquante pour cet abonnement : @erreur@. Veuillez le supprimer avant de poursuivre.",
+	'erreur_abonnement_beneficiaire' => "le bénéficiaire est inconnu",
+	'erreur_abonnement_numero_depart' => "le premier numéro est inconnu",
+	
+	// I
+	'icone_panier_titre' => "Vos achats",
+	
+	'label_afficher_panier' => "Afficher le contenu du panier",
 	'label_accepter_contrats'                   => "Avant de valider, veuillez accepter le contrat&nbsp;:",
+	
 	'message_panier_valide'                     => "Votre commande est validée",
-	'titre_icone_panier' => "Votre panier",
+	
+	// P
+	'page_panier_titre' => "Vos achats",
+	
 );
