296ebb651506c61353529f17c476a909e99be236
christophe le drean <christophe@rouge-rose.net>
2018-10-08 18:09:12 +0200

    coquille

--- lang/paquet-vpaniers_options_fr.php
+++ lang/paquet-vpaniers_options_fr.php
@@ -4,6 +4,6 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'vpaniers_options_nom' => "Vacarme, options du panier",
-	'vpaniers_options_slogan' => "Ajouter des options et des traitements spécifique au panier",
+	'vpaniers_options_slogan' => "Ajouter des options et des traitements spécifiques au panier",
 	'vpaniers_options_description' => "",
 );
